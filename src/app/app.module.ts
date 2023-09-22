bc790c9b5eb1dda3625c696ee0c10ed6599ad2fb
Waheire <arthurwaheire1@gmail.com>
2023-09-22 08:28:21 +0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Shared/header/header.component';
+import { FooterComponent } from './Components/Shared/footer/footer.component';
+import { HeroComponent } from './Components/Shared/hero/hero.component';
+import { AboutUsComponent } from './Components/Pages/about-us/about-us.component';
+import { WhatWeDoComponent } from './Components/Pages/what-we-do/what-we-do.component';
+import { OurServicesComponent } from './Components/Pages/our-services/our-services.component';
+import { SustainabilityComponent } from './Components/Pages/sustainability/sustainability.component';
+import { ContactComponent } from './Components/Pages/contact/contact.component';
+import { SubscribeComponent } from './Components/Shared/subscribe/subscribe.component';
+import { IntroductionComponent } from './Components/Shared/introduction/introduction.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutUsComponent,
+    WhatWeDoComponent,
+    OurServicesComponent,
+    SustainabilityComponent,
+    ContactComponent,
+    SubscribeComponent,
+    IntroductionComponent
+
   ],
   imports: [
     BrowserModule,
