a3157ea0b34920f679d08250bbb8ea9b5773f5bf
Waheire <arthurwaheire1@gmail.com>
2023-10-04 00:14:45 +0300

    UI Progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
- 
+    extend: {
+      colors: {
+        primary: "#00833E",
+        secondary: "#68B336",
+      },
+      fontFamily: {
+        bodyfont: ["Bebas Neue", "sans-serif"],
+      },
+    },
   },
   plugins: [require("flowbite")],
 };
