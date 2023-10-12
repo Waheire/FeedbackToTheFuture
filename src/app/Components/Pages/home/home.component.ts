c472aab95cff954f7756dd95b78f4107c3dda765
Waheire <arthurwaheire1@gmail.com>
2023-10-12 17:26:41 +0300

    UI Responsiveness

--- src/app/Components/Pages/home/home.component.ts
+++ src/app/Components/Pages/home/home.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  images = [
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1460627390041-532a28402358?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'nature1',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'nature2',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1640844444545-66e19eb6f549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+      imageAlt: 'person1',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'person2',
+    },
+  ]
+
+  texts = [
+    {
+      h4Heading: "Welcome To Feedback To the Future",
+      h1Heading: "CO-CREATION AND OPERATION OF CIRCULAR AND REGENERATIVE INNOVATIONS.",
+      pContentLeft: "50 Frmers in our Program",
+      pContentRight: "More than 200 Crops"
+    },
+    {
+      h4Heading: "Welcome To Feedback To the Future 2",
+      h1Heading: "CO-CREATION AND OPERATION OF CIRCULAR AND REGENERATIVE INNOVATIONS 2.",
+      pContentLeft: "50 Frmers in our Program 2",
+      pContentRight: "More than 200 Crops 2"
+    },
+    {
+      h4Heading: "Welcome To Feedback To the Future 3",
+      h1Heading: "CO-CREATION AND OPERATION OF CIRCULAR AND REGENERATIVE INNOVATIONS 3.",
+      pContentLeft: "50 Frmers in our Program 3",
+      pContentRight: "More than 200 Crops 3"
+    },
+    {
+      h4Heading: "Welcome To Feedback To the Future 4",
+      h1Heading: "CO-CREATION AND OPERATION OF CIRCULAR AND REGENERATIVE INNOVATIONS 4.",
+      pContentLeft: "50 Frmers in our Program 4",
+      pContentRight: "More than 200 Crops 4"
+    },
+  ]
 }
--- src/app/Components/Shared/carousel/carousel.component.ts
+++ src/app/Components/Shared/carousel/carousel.component.ts
@@ -1,27 +1,55 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { faCircleLeft } from '@fortawesome/free-solid-svg-icons';
 
-declare var $: any; //Import jQuery
+interface carouselImage {
+  imageSrc: string;
+  imageAlt: string;
+}
+
+interface imageText {
+  h4Heading: string;
+  h1Heading: string;
+  pContentLeft: string;
+  pContentRight: string;
+}
+
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent implements OnInit, AfterViewInit {
-  @ViewChild('owlCarousel') owlCarousel! : ElementRef;
+export class CarouselComponent {
 
-  ngAfterViewInit(): void {
-    $(this.owlCarousel.nativeElement).owlCarousel({
-      items:1,
-      loop:1,
-      autoplay:true,
-      autoplayTimeout:5000,
-      autoplayHoverPause: true,
-    });
+  @Input() images: carouselImage[] = [];
+  @Input() indicators = true;
+  @Input() controls = true;
+  @Input() texts: imageText[] = [];
+
+  selectedIndex = 0;
+  ngOnInit(): void {
+
   }
 
-  ngOnInit(): void {
-    
+  //sets index of image on dot/indicator click
+  selectedImage(index: number) {
+    this.selectedIndex = index;
+  }
+
+  onPrevClick() {
+    if (this.selectedIndex === 0) {
+      this.selectedIndex = this.images.length - 1;
+    } else {
+      this.selectedIndex--;
+    }
+  }
+
+  onNextClick() {
+    if (this.selectedIndex === this.images.length - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
   }
 
 }
